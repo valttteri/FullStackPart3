26f841f02a7d4319dae7c6181175ed3b8381a5e9
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2023-02-10 15:28:46 +0200

    puhelinluettelo full stack

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 morgan.token('postdata', (req) => {
 return req.method === "POST" ? JSON.stringify(req.body) : ' ';
 });
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'))
 
@@ -26,11 +30,11 @@ let persons = [
     number: '093-333-444'
   }
 ]
-
+//render all persons
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
-
+//get a single persons info
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
@@ -42,14 +46,17 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+//info for the address ./api/info
 let info =
 `<p>Phonebook has info for ${persons.length} people</p>
 <p>${new Date()}</p>`
 
+//render the info text
 app.get('/api/info', (req, res) => {
   res.send(info)
 })
 
+//delete a person
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
 
@@ -62,12 +69,10 @@ app.delete('/api/persons/:id', (req, res) => {
   
 })
 
+//add a new person
 app.post('/api/persons', (req, res) => {
   const body = req.body
-
   const intitialPersons = persons.map(person => person.name)
-  //console.log(intitialPersons)
-  //console.log(body.name)
 
   const person = {
     id: Math.floor(Math.random() * 100000),
@@ -75,6 +80,7 @@ app.post('/api/persons', (req, res) => {
     number: body.number,
   }
 
+  //check for mistakes in input pefore posting
   if (!body.name) {
     return res.status(400).json({ 
       error: 'name cannot be blank' 
@@ -90,12 +96,12 @@ app.post('/api/persons', (req, res) => {
       error: 'name already exists' 
     })
   }
-
+  //add a new person
   persons = persons.concat(person)
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
